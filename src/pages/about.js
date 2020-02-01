164701f8b9a0c38dcae7a31c52e6d7d84c240128
Jonathan Sexton <joncsexton@gmail.com>
2020-02-01 06:34:18 -0600

    testing things out

---
+++ src/pages/about.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from '../components/header';
+
+export default () => (
+    <>
+    <Header></Header>
+    <div className="bacon">
+        <h1 className="baconlover">
+            BACON BACON BACON!
+        </h1>
+    </div>
+    </>
+)
