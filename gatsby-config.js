eca081b59e6dda61e53ef4aa346573c84d750557
Jonathan Sexton <joncsexton@gmail.com>
2020-02-21 21:37:44 -0600

    removed all base components

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,12 +5,6 @@ module.exports = {
     author: `@gatsbyjs`,
   },
   plugins: [
-    {
-      resolve: `gatsby-plugin-typography`,
-      options: {
-        pathToConfigModule: `src/utils/typography`,
-      },
-    },
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
